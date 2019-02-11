35a1fadb6f3428990c9f9c4f7dc24f558f4c702e
easeava <tthd@163.com>
2019-02-11 11:13:00 +0800

    config

--- config/config.php
+++ config/config.php
@@ -9,4 +9,60 @@
  * file that was distributed with this source code.
  */
 
-return [];
+return [
+    'defaults' => [
+        'response_type' => 'array',
+
+        'use_laravel_cache' => true,
+
+        'log' => [
+            'level' => env('BAIDU_LOG_LEVEL', 'debug'),
+            'file' => env('BAIDU_LOG_FILE', storage_path('logs/baidu.log')),
+        ],
+    ],
+
+    'route' => [
+
+    ],
+
+    'bear_tp' => [
+        'default' => [
+            'client_id' => '',
+            'client_secret' => '',
+            'token' => '',
+            'aes_key' => '',
+        ],
+    ],
+
+    'bear' => [
+        'default' => [
+            'client_id' => '',
+            'client_secret' => '',
+            'token' => '',
+            'aes_key' => '',
+
+//            'oauth' => [
+//                'scopes' => ['snsapi_userinfo'],
+//                'callback' => '/oauth/callback',
+//            ],
+        ],
+    ],
+
+    'smart_tp' => [
+        'default' => [
+            'client_id' => '',
+            'token' => '',
+            'app_key' => '',
+            'aes_key' => '',
+        ],
+    ],
+
+    'smart_program' => [
+        'default' => [
+            'app_id' => '',
+            'app_secret' => '',
+            'app_key' => '',
+            'token' => '',
+        ],
+    ],
+];
